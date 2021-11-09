13e7f107f1b33a3f4d73b57ccf972f0c7cd8cbd2
simonschaluppe <simon.schneider@technikum-wien.at>
2021-11-09 13:46:37 +0100

    adds rudimentary plot

--- utils/plotting.py
+++ utils/plotting.py
@@ -15,20 +15,23 @@ def xy_balance_plot(
 ):
     #plot targets
     gfzs = np.array([0,0.2,0.4,0.6, 0.8,
-                     1.,1.25,1.5,1.75,
-                     2.,2.5,3,4,6])
+                     1.,1.5,
+                     2.,3,5])
     target_class = ZQSynergy()
     targets = target_class.alpha_zielwert_bgf(gfzs)
-    for dy in targets:
-        x, y = target_line(dy, xmax, ymax)
-        ax.plot(x,y,"--", color="grey",label="asdsad")
+    for gfz,dy in zip(gfzs,targets):
+        tx, ty = target_line(dy, xmax, ymax)
+        ax.plot(tx,ty,"--",
+                linewidth=0.5,
+                color="grey",
+                )
+        ax.text(xmax*0.5-dy/2, xmax*0.75-xmax*0.25+dy/2,f"FSI {round(gfz,2)}", size=7, rotation=45)
+        ax.text(xmax*0.5-dy/2+20, xmax*0.75-xmax*0.25+dy/2+20,f"target {round(dy,2)}", size=5, rotation=45)
         #TODO: annotate lines with gfz
 
 
     #plot points
-    x = [140,110,80, 60, 55, 51]
-    y = [0,0,20, 30, 35, 35]
-    ax.plot(x,y,"o--", label="asdsad")
+
 
 
     # axis formatting
@@ -36,7 +39,7 @@ def xy_balance_plot(
     ax.set(xlim=(0,xmax))
     ax.set(ylabel="PED Primary Energy Export $[kWh/m²_{GFA}]$")
     ax.set(xlabel="PED Primary Energy Import $[kWh/m²_{GFA}]$")
-    #TODO: noborder
+    #TODO: noborderK
     #TODO: draw x and y axis as arrows
 
 
@@ -49,9 +52,57 @@ def target_line(
     y = np.arange(0, ymax, 10) + offset
     return x, y
 
+def example_xy_balance():
+    fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+    xy_balance_plot(ax)
+
+    x = [[140, 110, 80, 60, 55, 51]],
+    y = [0, 0, 20, 30, 35, 35],
+    ax.plot(x, y, "--ob", linewidth=2, linestyle="dashed")
+    fig.show()
+
+def demand_supply_balance(ax, data_dict, title=None):
+    """
+    :param ax: the matplotlib ax to draw on
+    :param data_dict: dictionary with keys "demand"=[vals], "supply"=[supplies]
+    :param title: string
+    :return:
+    """
+    abs=0
+    for l, x in data_dict["demand"].items():
+        ax.bar(data_dict.keys(), [x, 0], bottom=abs, label=l)
+        abs += x
+        # print(f"plotting {l=}, {x=}")
+
+    abs=0
+    for l, x in data_dict["supply"].items():
+        ax.bar(data_dict.keys(), [0, x], bottom=abs, label=l)
+        abs += x
+
+    ax.set(title=title)
+
+
+def example_demand_supply_bars():
+    balances = [
+        dict(
+            demand=dict(HVAC=45, Plugloads=33, Other=10),
+            supply=dict(PeakShaving=8.1, PVdirectuse=50, PV_grid_offset=20, densitycontext=20)
+        ),
+        dict(
+            demand=dict(HVAC=25, Plugloads=33, Other=5),
+            supply=dict(PeakShaving=8.1, PVdirectuse=15, PV_grid_offset=20, densitycontext=20)
+        )
+    ]
+
+    fig, ax = plt.subplots(1, len(balances), figsize=(6, 6), sharey=True)
+    for i, balance in enumerate(balances):
+        demand_supply_balance(ax[i], balance)
+
+    ax[-1].legend()
+
+    fig.show()
 
 if __name__ == "__main__":
-    fig, ax = plt.subplots(1,1, figsize=(6,6))
-    xy_balance_plot(ax)
-    fig.show()
+    example_demand_supply_bars()
+
 
