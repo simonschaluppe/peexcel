60485e52215d76c5aabeb5622ca0c2a6a17f9c7a
simonschaluppe <simon.schneider@technikum-wien.at>
2025-04-03 13:39:43 +0200

    repository cleanup

--- utils/datahandling.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-DATA_PATH       = Path("../data/")
-EMA_PATH        = Path("../data/Energiemosaik_Datenpaket_AT/")
-EMA_EXCEL_PATH  = Path(EMA_PATH, "0_AT_Energiemosaik.xlsx")
